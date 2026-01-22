31d3947e49fc597754b2cfb136cc4284c493105e
jamasaurus-hub <jdathornton@gmail.com>
2026-01-22 23:54:13 +0000

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3500",
+  baseURL: "https://lla-mvp-backend-production.up.railway.app",
 });
 
 export default instance;
